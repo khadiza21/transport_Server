342c234c73220e1b3f0a94fefe623c3251cf4eb7
khadiza21 <bibikhadiza474@gmail.com>
2024-05-05 21:04:50 +0600

    update

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res) => {
-    res.send('mental application')
-})
+app.get("/", (req, res) => {
+  res.send("mental application");
+});
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@trassyes.mixiffr.mongodb.net/?retryWrites=true&w=majority&appName=trassyes`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,30 +29,103 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-   
-    const reviewCollection = client.db("trasportsytem").collection("review");
-    const categoriesCollection = client.db("trasportsytem").collection("categories");
-    const cartypesCollection = client.db("trasportsytem").collection("cartypes");
 
-    app.get('/review', async(req, res) => {
+    const reviewCollection = client.db("trasportsytem").collection("review");
+    const categoriesCollection = client
+      .db("trasportsytem")
+      .collection("categories");
+    const cartypesCollection = client
+      .db("trasportsytem")
+      .collection("cartypes");
+    const userCollection = client.db("trasportsytem").collection("users");
+    const busDriverCollection = client
+      .db("trasportsytem")
+      .collection("busdriveraccount");
+    const carDriverCollection = client
+      .db("trasportsytem")
+      .collection("cardriveraccount");
+    const aboutCart = client.db("trasportsytem").collection("aboutcart");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.post("/busdriveraccount", async (req, res) => {
+      const busdriver = req.body;
+      const query = { email: busdriver.email };
+      const existingBusDriver = await busDriverCollection.findOne(query);
+      if (existingBusDriver) {
+        return res.send({ message: " already exists", insertedId: null });
+      }
+      const result = await busDriverCollection.insertOne(busdriver);
+      res.send(result);
+    });
+
+    // app.post("/cardriveraccount", async (req, res) => {
+    //   const cardriver = req.body;
+    //   const query = { email: cardriver.email };
+    //   const existingBusDriver = await carDriverCollection.findOne(query);
+    //   if (existingBusDriver) {
+    //     return res.send({ message: " already exists", insertedId: null });
+    //   }
+    //   const result = await carDriverCollection.insertOne(cardriver);
+    //   res.send(result);
+    // });
+
+    app.get("/users", async (req, res) => {
+      const userlists = await userCollection.find().toArray();
+      res.send(userlists);
+    });
+
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id:  id };
+      console.log(query);
+      const result = await userCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
+
+    app.get("/busdriveraccount", async (req, res) => {
+      const busdriverlist = await busDriverCollection.find().toArray();
+      res.send(busdriverlist);
+    });
+
+    app.get("/busdriveraccount/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const result = await busDriverCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/review", async (req, res) => {
       const revresult = await reviewCollection.find().toArray();
       res.send(revresult);
-    })
-    
+    });
 
-   
-    app.get('/categories', async(req, res) => {
+    app.get("/aboutcart", async (req, res) => {
+      const aboutc = await aboutCart.find().toArray();
+      res.send(aboutc);
+    });
+
+    app.get("/categories", async (req, res) => {
       const catresult = await categoriesCollection.find().toArray();
       res.send(catresult);
-    })
+    });
 
-
-    app.get('/cartypes', async(req, res) => {
+    app.get("/cartypes", async (req, res) => {
       const cartypesresult = await cartypesCollection.find().toArray();
       res.send(cartypesresult);
-    })
-   
-   
+    });
+
     console.log("connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -62,6 +134,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.listen(port, ()=> {
-    console.log(`this is bk ${port}`);
-})
+app.listen(port, () => {
+  console.log(`this is bk ${port}`);
+});
