ca498f75a5685c7bb3cd9445a4c0c13322918452
khadiza21 <bibikhadiza474@gmail.com>
2024-05-06 14:25:19 +0600

    update

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
   res.send("mental application");
 });
 
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@trassyes.mixiffr.mongodb.net/?retryWrites=true&w=majority&appName=trassyes`;
+
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -22,6 +25,8 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+
 
 async function run() {
   try {
@@ -46,6 +51,9 @@ async function run() {
       .collection("cardriveraccount");
     const aboutCart = client.db("trasportsytem").collection("aboutcart");
 
+
+    //  for user and admin 
+    // crate user admin a
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
@@ -56,7 +64,26 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+//find all users and admin
+    app.get("/users", async (req, res) => {
+      const userlists = await userCollection.find().toArray();
+      res.send(userlists);
+    });
+//find one user/admin 
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: id };
+      console.log(query);
+      const result = await userCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
 
+
+
+
+    // busdriver
     app.post("/busdriveraccount", async (req, res) => {
       const busdriver = req.body;
       const query = { email: busdriver.email };
@@ -65,32 +92,6 @@ async function run() {
         return res.send({ message: " already exists", insertedId: null });
       }
       const result = await busDriverCollection.insertOne(busdriver);
-      res.send(result);
-    });
-
-    // app.post("/cardriveraccount", async (req, res) => {
-    //   const cardriver = req.body;
-    //   const query = { email: cardriver.email };
-    //   const existingBusDriver = await carDriverCollection.findOne(query);
-    //   if (existingBusDriver) {
-    //     return res.send({ message: " already exists", insertedId: null });
-    //   }
-    //   const result = await carDriverCollection.insertOne(cardriver);
-    //   res.send(result);
-    // });
-
-    app.get("/users", async (req, res) => {
-      const userlists = await userCollection.find().toArray();
-      res.send(userlists);
-    });
-
-    app.get("/users/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log(id);
-      const query = { _id:  id };
-      console.log(query);
-      const result = await userCollection.findOne(query);
-      console.log(result);
       res.send(result);
     });
 
@@ -106,24 +107,50 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/review", async (req, res) => {
-      const revresult = await reviewCollection.find().toArray();
-      res.send(revresult);
+
+
+    // cardriver
+    app.post("/cardriveraccount", async (req, res) => {
+      const cardriver = req.body;
+      const query = { email: cardriver.email };
+      const existingCarDriver = await carDriverCollection.findOne(query);
+      if (existingCarDriver) {
+        return res.send({ message: " already exists", insertedId: null });
+      }
+      const result = await carDriverCollection.insertOne(cardriver);
+      res.send(result);
     });
 
-    app.get("/aboutcart", async (req, res) => {
-      const aboutc = await aboutCart.find().toArray();
-      res.send(aboutc);
+    app.get("/cardriveraccount", async (req, res) => {
+      const cardriverlist = await carDriverCollection.find().toArray();
+      res.send(cardriverlist);
     });
 
+
+    // car items types 
+    app.get("/cartypes", async (req, res) => {
+      const cartypesresult = await cartypesCollection.find().toArray();
+      res.send(cartypesresult);
+    });
+
+
+    // service category
     app.get("/categories", async (req, res) => {
       const catresult = await categoriesCollection.find().toArray();
       res.send(catresult);
     });
 
-    app.get("/cartypes", async (req, res) => {
-      const cartypesresult = await cartypesCollection.find().toArray();
-      res.send(cartypesresult);
+
+    // all reviews 
+    app.get("/review", async (req, res) => {
+      const revresult = await reviewCollection.find().toArray();
+      res.send(revresult);
+    });
+
+    // about page data
+    app.get("/aboutcart", async (req, res) => {
+      const aboutc = await aboutCart.find().toArray();
+      res.send(aboutc);
     });
 
     console.log("connected to MongoDB!");
