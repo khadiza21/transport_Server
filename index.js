3dbe98ebb894c0848b615a4698186fda50b8a1fe
khadiza21 <bibikhadiza474@gmail.com>
2024-06-16 22:11:11 +0600

    bus service component update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("mental application");
+  res.send("sad application");
 });
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
